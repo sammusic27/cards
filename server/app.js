e6c392eb18c76bd9f4f834bd938f6c8dd8318d67
Stanislav Mashyn <sammusicdnb@gmail.com>
2018-01-31 08:35:44 +0200

    initial commit

---
+++ server/app.js
@@ -0,0 +1,68 @@
+const WebSocketServer = new require('ws');
+const buildDeck = require('./service/common/operations').buildDeck;
+
+// подключенные клиенты
+const clients = {};
+
+// WebSocket-сервер на порту 8081
+const webSocketServer = new WebSocketServer.Server({
+    port: 8081
+});
+webSocketServer.on('connection', function(ws) {
+    const id = Math.random();
+    clients[id] = ws;
+    console.log("новое соединение " + id);
+
+    ws.on('message', function(message, a) {
+        console.log('получено сообщение ' + message);
+
+        const data = parseData(message);
+        if(!data) return;
+
+        detectType(data, ws);
+
+    });
+
+    ws.on('close', function() {
+        console.log('соединение закрыто ' + id);
+        delete clients[id];
+    });
+
+    ws.on('error', function (msg) {
+        console.log(msg)
+    });
+});
+
+function parseData(request){
+    let data;
+    try{
+        data = JSON.parse(request);
+        if(!data['$type']){
+            data = null;
+        }
+    }catch(e){
+        console.log('error by ' + request, e);
+    }
+
+    return data;
+}
+
+
+function detectType(data, ws){
+    switch(data['$type']){
+        case 'ping': ws.send(JSON.stringify({'$type': 'ping', 'message': 'pong'})); break;
+        case 'clients':
+            const keys = Object.keys(clients);
+            ws.send(JSON.stringify({'$type': 'clients', 'message': keys}));
+            break;
+        case 'all':
+            for (const key in clients) {
+                clients[key].send(JSON.stringify({'$type': 'message', message: data.message}));
+            }
+            break;
+        case 'build.deck':
+            const deck = buildDeck['54']();
+            ws.send(JSON.stringify({'$type': 'deck', 'deck': deck}));
+            break;
+    }
+}
---
+++ server/constants/cards.js
@@ -0,0 +1,68 @@
+
+module.exports.cards = {
+    2: {
+        name: '2',
+        suit: '',
+        rank: 1
+    },
+    3: {
+        name: '3',
+        suit: '',
+        rank: 2
+    },
+    4: {
+        name: '4',
+        suit: '',
+        rank: 3
+    },
+    5: {
+        name: '5',
+        suit: '',
+        rank: 4
+    },
+    6: {
+        name: '6',
+        suit: '',
+        rank: 5
+    },
+    7: {
+        name: '7',
+        suit: '',
+        rank: 6
+    },
+    8: {
+        name: '8',
+        suit: '',
+        rank: 7
+    },
+    9: {
+        name: '9',
+        suit: '',
+        rank: 8
+    },
+    10: {
+        name: '10',
+        suit: '',
+        rank: 9
+    },
+    'J': {
+        name: 'Jack',
+        suit: '',
+        rank: 10
+    },
+    'Q': {
+        name: 'Queen',
+        suit: '',
+        rank: 11
+    },
+    'K': {
+        name: 'King',
+        suit: '',
+        rank: 12
+    },
+    'A': {
+        name: 'Ace',
+        suit: '',
+        rank: 13
+    },
+};
